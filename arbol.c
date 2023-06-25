8bda37176ce9213201ceeb679300f6da2f4aef39
makmadon <stlaintotofranco@gmail.com>
2023-06-25 00:21:03 -0400

    Signed-off-by: makmadon <stlaintotofranco@gmail.com>
    Implementacion de crear nodo

---
+++ arbol.c
@@ -0,0 +1,11 @@
+#include "arbol.h"
+
+static Node* CreaNodo(char letra, char *significado){
+    Node *newp;
+    if((newp=(Node*)malloc(sizeof(Node)))==NULL)
+        return NULL;
+    newp->letra=letra;
+    strcpy(newp->significado,significado);
+    newp->hijos=NULL;
+    newp->next=NULL;
+}
